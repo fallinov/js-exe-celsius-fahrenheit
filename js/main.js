77855c096cb519493e10397e88dcb66f3b17642d
Steve Fallet <steve.fallet@divtec.ch>
2023-09-06 18:10:44 +0200

    version

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
  * @author  Steve Fallet
- * @version 0.1
- * @since   2019-08-19
+ * @version 1.1
+ * @since   2023-09-06
  */
   'use strict';
 
@@ -10,6 +10,7 @@
   let celcius = prompt('Entrez la température en °C :');
   // Convertir la saisie utilisateur en nombre flottant
   celcius = parseFloat(celcius);
+
   // Vérifier que la saisie utilisateur est un nombre
   if (isNaN(celcius)) {
     // Afficher un message d'erreur
@@ -17,6 +18,7 @@
     // Arrêter le script
     throw new Error('La valeur saisie n\'est pas un nombre !'); // Erreur fatale
   }
+
   // Calculer la température en °F
   let fahrenheit = celcius * 9 / 5 + 32;
   // Afficher la température en °F
